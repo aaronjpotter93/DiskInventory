9a06ee88c4232abfbfe433cc3c006a6e38ea9a2e
apotter <apotter@SERVERB10>
2022-04-15 15:10:05 -0600

    Add project files.

---
+++ DiskInventory/Controllers/DiskController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using DiskInventory.Models;
+
+namespace DiskInventory.Controllers
+{
+    public class DiskController : Controller
+    {
+        private disk_inventoryapContext context { get; set; }
+        public DiskController(disk_inventoryapContext ctx)
+        {
+            context = ctx;
+        }
+        public IActionResult Index()
+        {
+            List<Disk> disks = context.Disks.ToList();
+            return View(disks);
+        }
+    }
+}
